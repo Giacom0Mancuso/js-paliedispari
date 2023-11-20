1c02aa912fc930da5276e9df6abc9cfd0568a79f
Giacom0Mancuso <giacomomancuso35@email.com>
2023-11-20 16:29:06 +0100

    solved isue

--- js/main3.js
+++ js/main3.js
@@ -2,7 +2,7 @@ let arrey = [1,2,3,4,5,6,7,2,3,6];
 
 function arreySomma(arrey){
     let somma = 0;
-    for(let i = 0; i < arrey.legnth; i ++){
+    for(let i = 0; i < arrey.length; i ++){
         somma += arrey[i];
     }
     return somma;

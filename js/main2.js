7e4ef477f03b5fcd94c79577aace152445ca9f2a
Giacom0Mancuso <giacomomancuso35@email.com>
2023-11-20 15:34:19 +0100

    odd or even check done

--- js/main2.js
+++ js/main2.js
@@ -1,4 +1,15 @@
 let num = prompt("inserisci un numero da 1 a 5");
 
-let num2 = Math.floor((Math.random() * 5) + 1);
+num = parseInt(num);
 
+let num2 = Math.floor((Math.random() * 5) + 1);
+console.log(num2);
+
+let oddOrEven = num + num2;
+
+if(oddOrEven % 2 == 0){
+    console.log("vince pari");
+}
+else{
+    console.log("vince dispari");
+}
